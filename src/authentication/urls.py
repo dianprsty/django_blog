b233948a5a3a16725292591d10d85a461553ceca
dianprsty <dprasmail@gmail.com>
2024-02-23 23:27:32 +0700

    UI for auth, landing, blog

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.base, name = "authentication")
+    path("login/", views.base, name = "login"),
+    path("register/", views.register, name = "register"),
 ]
--- src/authentication/views.py
+++ src/authentication/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def base(request):
-    return render(request, "authentication/index.html")
+    return render(request, "authentication/login.html")
+
+def register(request):
+    return render(request, "authentication/register.html")
